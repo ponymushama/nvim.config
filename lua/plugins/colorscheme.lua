0ea0c522ba65d89d4f11b653f07dd9ff9d4d994e
ponymushama <ponymushama@gmail.com>
2024-03-12 11:34:39 +0800

    delete useless plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,8 @@ return {
   -- add colorscheme
   {
     "sainnhe/gruvbox-material",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
     -- config colorscheme
     config = function()
       vim.g.gruvbox_material_enable_italic = 1
