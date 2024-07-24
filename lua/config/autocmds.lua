b93466e93fb33b1689220a93f50b98bc42e75c7d
ponymushama <ponymushama@gmail.com>
2024-07-24 21:51:39 +0800

    update im switch

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,33 +2,29 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- 自动切换输入法，需要安装 im-select
--- https://github.com/daipeihust/im-select
+-- im switch
+local autocmd = vim.api.nvim_create_autocmd
+
 local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
   clear = true,
 })
 
-local autocmd = vim.api.nvim_create_autocmd
-local system = vim.loop.os_uname().sysname
-
-if system == "Darwin" then
-  autocmd("InsertLeave", {
-    group = myAutoGroup,
-    callback = require("utils.im-select").macInsertLeave,
-  })
-  autocmd("InsertEnter", {
-    group = myAutoGroup,
-    callback = require("utils.im-select").macInsertEnter,
-  })
-  autocmd("FocusGained", {
-    group = myAutoGroup,
-    callback = require("utils.im-select").macFocusGained,
-  })
-  autocmd("FocusLost", {
-    group = myAutoGroup,
-    callback = require("utils.im-select").macFocusLost,
-  })
-end
+autocmd("InsertLeave", {
+  group = myAutoGroup,
+  callback = require("utils.im-select").macInsertLeave,
+})
+autocmd("InsertEnter", {
+  group = myAutoGroup,
+  callback = require("utils.im-select").macInsertEnter,
+})
+autocmd("FocusGained", {
+  group = myAutoGroup,
+  callback = require("utils.im-select").macFocusGained,
+})
+autocmd("FocusLost", {
+  group = myAutoGroup,
+  callback = require("utils.im-select").macFocusLost,
+})
 
 -- toggle markdown conceal
 autocmd("FileType", {
