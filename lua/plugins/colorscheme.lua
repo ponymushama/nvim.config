00e0425e6f5cb9fcb7dcd69ad72657ebc31ff470
ponymushama <ponymushama@gmail.com>
2024-12-29 18:33:43 +0800

    set colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,19 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = true,
+    lazy = false,
     priority = 1000,
     name = "catppuccin",
     opts = {
+      transparent_background = true,
       integrations = {
         aerial = true,
         alpha = true,
         cmp = true,
         dashboard = true,
         flash = true,
+        fzf = true,
+        grug_far = true,
         gitsigns = true,
         headlines = true,
         illuminate = true,
@@ -36,10 +39,22 @@ return {
         noice = true,
         notify = true,
         semantic_tokens = true,
+        snacks = true,
         telescope = true,
         treesitter = true,
         treesitter_context = true,
         which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
       },
     },
   },
@@ -47,51 +62,47 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
 
---[[
-return {
-  -- add colorscheme
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- config colorscheme
-    config = function()
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_background = "soft"
-    end,
-  },
-  -- Configure LazyVim to load colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox-material",
-    },
-  },
-}
---]]
+-- return {
+--   -- add colorscheme
+--   {
+--     "sainnhe/gruvbox-material",
+--     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--     priority = 1000, -- make sure to load this before all the other start plugins
+--     -- config colorscheme
+--     config = function()
+--       vim.g.gruvbox_material_enable_italic = 1
+--       vim.g.gruvbox_material_background = "soft"
+--     end,
+--   },
+--   -- Configure LazyVim to load colorscheme
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox-material",
+--     },
+--   },
+-- }
 
---[[
-return {
-  -- add colorscheme
-  {
-    "sainnhe/everforest",
-    -- config colorscheme
-    config = function()
-      vim.g.everforest_enable_italic = 1
-      vim.g.everforest_background = "soft"
-    end,
-  },
-  -- Configure LazyVim to load colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "everforest",
-    },
-  },
-}
---]]
+-- return {
+--   -- add colorscheme
+--   {
+--     "sainnhe/everforest",
+--     -- config colorscheme
+--     config = function()
+--       vim.g.everforest_enable_italic = 1
+--       vim.g.everforest_background = "soft"
+--     end,
+--   },
+--   -- Configure LazyVim to load colorscheme
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "everforest",
+--     },
+--   },
+-- }
